1f4e9112cd454cdc1adb1907685776edcbc9f8b1
juanfelipe86 <69727823+juanfelipe86@users.noreply.github.com>
2021-03-23 23:30:34 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Recursion
+{
+    class Program
+    {
+        static int sumaRecursiva (int n)
+        {
+            if (n == 1)
+                return 1;
+            else
+                return n + sumaRecursiva(n-1);
+
+        }
+
+        static void Main (string[] args)
+        {
+            int n;
+
+            Console.Write(" Ingresar los numeros a sumar: ");
+            n = int.Parse(Console.ReadLine() );
+
+            Console.Write(" La suma de los numeros es : " + sumaRecursiva(n) );
+           Console.ReadKey();
+        }  
+    }
+}
